9993f782ef4c65b7c4b0693da538e54e4d8a99b9
Jenny Lien <clienjenny@gmail.com>
2020-04-25 23:53:33 +0000

    add first cut of the website template

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,13 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 from processing import do_calculation
 
 app = Flask(__name__)
-add.config["DEBUG"] = True
-@app.route('/')
-def hello_world():
-    return 'Jenny testing'
+app.config["DEBUG"] = True
 
+
+@app.route('/')
+def index():
+    return render_template("main_page.html")
